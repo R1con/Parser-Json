e26603d6461c92d16f70ff2be8abef88337cf174
R1con <igor.prilutzkij@yandex.ru>
2022-04-13 14:43:33 +0300

    Initial commit

---
+++ src/main/java/IP.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+class IP implements Serializable {
+
+    private String ip;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
---
+++ src/main/java/ParserJson.java
@@ -0,0 +1,44 @@
+import com.google.gson.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import org.jsoup.select.Elements;
+
+import java.io.*;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+class ParserJson {
+    private static IP ip = new IP();
+
+    public static void main(String[] args) throws IOException {
+        String url = "https://api.ipify.org/?format=json";
+        String path = "C:\\Users\\igorp\\Desktop\\ip-info.txt";
+
+        parseFromUrl(url);
+        writeJsonObjectToFile(path, ip.getIp());
+    }
+
+    public static void parseFromUrl(String url) throws IOException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        Document document = Jsoup.connect(url).ignoreContentType(true).get();
+        Elements elements = document.select("body");
+        String stringJson = elements.text();
+
+        ip = gson.fromJson(stringJson, IP.class);
+    }
+
+    public static void writeJsonObjectToFile(String path, Object obj) {
+        try (BufferedWriter writerFile = Files.newBufferedWriter(Path.of(path), StandardCharsets.UTF_8)) {
+            writerFile.write(String.valueOf(obj));
+            System.out.println("File wrote in " + path);
+        } catch (IOException ex) {
+            System.err.println("Error: " + ex.getMessage());
+        }
+    }
+}
