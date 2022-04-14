6d96bb05d3de154174f03447900440d59ec46e4f
R1con <igor.prilutzkij@yandex.ru>
2022-04-14 16:45:40 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,15 @@ import writer.FileWrite;
 import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         ParserJson parser = new ParserJson();
         FileWrite fileWrite = new FileWrite();
 
-        String url = "https://api.ipify.org/?format=json";
-        String path = "C:\\Users\\igorp\\Desktop\\ip-info.txt";
+        final String URl = "https://api.ipify.org/?format=json";
+        final String PATH = "C:\\Users\\igorp\\Desktop\\ip-info.txt";
 
 
-        IpDto ipDto = parser.parseFromUrlToJsonObject(url);
-        fileWrite.writeJsonObjectToFile(path, ipDto.getIp());
+        IpDto ipDto = parser.parseFromUrlToJsonObject(URl);
+        fileWrite.writeJsonObjectToFile(PATH, ipDto.getIp());
     }
 }
