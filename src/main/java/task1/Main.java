44a5de6f3a2d414ad352e0707358728e387f51c2
R1con <igor.prilutzkij@yandex.ru>
2022-04-20 14:29:51 +0300

    init

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -9,29 +9,50 @@ import task1.writer.FileWriter;
 import java.io.IOException;
 
 public class Main {
-    private static ParserStrategy parserStrategy = null;
-    private static IpDto ipDto = null;
-
     private static final FileWriter FILE_WRITER = new FileWriter();
+    private static ParserStrategy parserStrategy;
+    private static IpDto ipDto;
 
     public static void main(String[] args) throws IOException {
-        final String ARGUMENT_URL = "url";
-        final String ARGUMENT_FILE = "file";
+        final String ARGUMENT_URL = TypeOfDataStorage.URL.getType();
+        final String ARGUMENT_FILE = TypeOfDataStorage.FILE.getType();
 
-        if (args[0].equals(ARGUMENT_URL)) {
-            parserStrategy = new JsonParser();
-            ChoosingStrategy(parserStrategy);
-        } else if (args[0].equals(ARGUMENT_FILE)) {
-            parserStrategy = new FileParser();
-            ChoosingStrategy(parserStrategy);
+        if (args.length != 0) {
+            if (args[0].equals(ARGUMENT_URL)) {
+                parserStrategy = new JsonParser();
+            } else if (args[0].equals(ARGUMENT_FILE)) {
+                parserStrategy = new FileParser();
+            } else {
+                System.out.println("Incorrect input. Input url or file.");
+            }
         } else {
-            System.out.println("Incorrect input. Input url or file.");
+            System.out.println("Input argument url or file.");
         }
+
+        try {
+            ipDto = parserStrategy.parseJson();
+            FILE_WRITER.writeToFile(ipDto.getIp());
+        } catch (NullPointerException e) {
+            e.getMessage();
+        }
+
 
     }
 
-    private static void ChoosingStrategy(ParserStrategy parserStrategy) throws IOException {
-        ipDto = parserStrategy.parseJson();
-        FILE_WRITER.writeToFile(ipDto.getIp());
+
+}
+
+enum TypeOfDataStorage {
+    URL("url"),
+    FILE("file");
+
+    private final String type;
+
+    TypeOfDataStorage(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
     }
 }
